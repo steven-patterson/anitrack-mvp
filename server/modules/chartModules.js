a96291e7cbea351cf8f011cb2723cfbde53126b1
Steven Patterson <spatterson.dev@gmail.com>
2015-08-07 13:21:02 -0400

    Removed excess test code and added comments

--- server/modules/chartModules.js
+++ server/modules/chartModules.js
@@ -1,8 +1,6 @@
 var cheerio = Meteor.npmRequire("cheerio");
 var request= Meteor.npmRequire('superagent');
 
-var animeTitles = null;
-var animeImages = null;
 var cardsObj = {};
 var animeHtml = "";
 
@@ -10,18 +8,19 @@ Meteor.startup(function(){
 	var url = "http://www.anime-planet.com/anime/seasons/summer-2015";
 	var user_agent = 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14';
 
+	//Issue request to anime data site
 	request
 		.get(url)
 		.set('User-Agent', user_agent)
 		.end(function(err, res){
 			$ = cheerio.load(res.text);
+			//Find all anime titles on page
 			animeTitles = $('.card').find("h4").map(function(i, el) {
+				//Grab the associated image and pair it to title
 				var animeImage = $(this).siblings().find("div.crop.portrait img").attr("data-src");
+				//Set the title as a key and image as a value
 				cardsObj[$(this).text()] = animeImage;
 				return $(this).text();
-			}).get().join(", ");
-			animeImages = $(".card").find("div.crop.portrait img").map(function(i, el) {
-				return $(this).attr("data-src");
 			}).get().join(", ");
 		});
 });
@@ -29,8 +28,6 @@ Meteor.startup(function(){
 Meteor.methods({
 	//Scrape anime chart data from external websites
 	getChartData: function () {
-		var animeArr = animeTitles.split(",");
-		var imageArr = animeImages.split(",");
 		for (var key in cardsObj) {
 			animeHtml += "<li>" + key + "<br />"
 			+ "<img src='http://www.anime-planet.com" + cardsObj[key] + "'></li>";
