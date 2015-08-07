a45fff4db6002f31ff88f125e6e7d1f1a80f54ea
Steven Patterson <spatterson.dev@gmail.com>
2015-08-07 03:33:38 -0400

    Created attach time function

--- server/modules/chartModules.js
+++ server/modules/chartModules.js
@@ -13,12 +13,12 @@ Meteor.startup(function(){
 		.end(function(err, res){
 			$ = cheerio.load(res.text);
 			animeTitle = $('.card').find("h4").map(function(i, el) {
-						var animeImage = $(this).siblings().find("div.crop.portrait img").attr("data-src");
-						var animeImageHtml = "<img src='http://www.anime-planet.com" + animeImage + "'>";
-						console.log(animeImage);
-						//Format the results into an HTML list style with each entry as a separate li
-						return "<li>" + $(this).text()+ "</li><br />" + animeImageHtml;
-					}).get().join(' '); //Join all li together
+				var animeImage = $(this).siblings().find("div.crop.portrait img").attr("data-src");
+				var animeImageHtml = "<img src='http://www.anime-planet.com" + animeImage + "'>";
+				attachTime($(this).text());
+				//Format the results into an HTML list style with each entry as a separate li
+				return "<li>" + $(this).text()+ "</li><br />" + animeImageHtml;
+			}).get().join(' '); //Join all li together
 		});
 });
 
@@ -28,3 +28,20 @@ Meteor.methods({
 		return animeTitle;
 	}
 });
+
+function attachTime(animeString) {
+	request
+		.get("https://www.livechart.me/schedule/tv")
+		.set("User-Agent", 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14')
+		.end(function(err, res) {
+			$ = cheerio.load(res.text);
+			var animeCard = $('.anime-card').map(function(i, el) {
+				var airTime = $(this).find(".poster-wrap > .episode-countdown").html();
+				var mainTitle = $(this).find(".main-title").html();
+				if (animeString.substr(0, 5).indexOf(mainTitle.substr(0, 5)) !== -1) {
+					console.log(mainTitle + " == " + animeString);
+					console.log(airTime);
+				}
+			});
+		});
+}
