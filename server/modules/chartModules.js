d3b2c190cb0790482415c5eb25ca8ee55b3fcf4b
Steven Patterson <spatterson.dev@gmail.com>
2015-08-08 00:04:23 -0400

    Added zombie for image fetching

--- server/modules/chartModules.js
+++ server/modules/chartModules.js
@@ -1,9 +1,9 @@
 var cheerio = Meteor.npmRequire("cheerio");
 var request= Meteor.npmRequire('superagent');
+var Browser = Meteor.npmRequire("zombie");
 
 var cardsObj = {};
 var animeHtml = "";
-
 
 Meteor.startup(function(){
 	var url = "http://www.anime-planet.com/anime/seasons/summer-2015";
@@ -33,7 +33,19 @@ Meteor.methods({
 			animeHtml += "<li>" + key + "<br />"
 			+ "<img src='http://www.anime-planet.com" + cardsObj[key] + "'></li>";
 		}
-
+		searchAnime("Railgun");
 		return animeHtml;
 	}
 });
+
+function searchAnime(animeName) {
+	var url = "https://hummingbird.me/anime";
+	var user_agent = 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14';
+	var browser = new Browser({userAgent: user_agent, debug: true, waitFor: 10000});
+
+	browser.visit(url, function() {
+		var result = browser.html();
+		console.log(animeName);
+		console.log(result);
+	});
+}
