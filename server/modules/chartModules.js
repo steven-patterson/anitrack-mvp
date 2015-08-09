d547a572de711cccb6119d4a6b58fc8407004c88
Steven Patterson <spatterson.dev@gmail.com>
2015-08-09 17:36:12 -0400

    Refactored code to objects

--- server/modules/chartModules.js
+++ server/modules/chartModules.js
@@ -1,48 +1,32 @@
 var cheerio = Meteor.npmRequire("cheerio");
-var request= Meteor.npmRequire('superagent');
-var Browser = Meteor.npmRequire("zombie");
 
-var cardsObj = {};
+var animeArray = [];
 var animeHtml = "";
-var imagesHtml = "";
 
-Meteor.startup(function(){
+function Anime (animeName, airTime) {
+	this.animeName = animeName;
+	this.airTime = airTime;
+}
+
+Meteor.startup(function() {
 	var url = "https://www.livechart.me/schedule/tv";
-	var user_agent = 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14';
-
-	//Issue request to anime data site
-	request
-		.get(url)
-		.set('User-Agent', user_agent)
-		.end(function(err, res){
-			$ = cheerio.load(res.text);
-			//Find all anime titles on page
-			var animeTitles = $('div.anime-card').find("h3").map(function(i, el) {
-				//Find the associated air time and set it
-				var airTime = $(this).siblings().find("div.poster-wrap div.episode-countdown").text();
-				cardsObj[$(this).text()] = airTime;
-				//Search for anime image, if it exists, return it and set as img src
-				request
-					.get("http://www.anime-planet.com/anime/all")
-					.set("User-Agent", user_agent)
-					.query({name: $(this).text()})
-					.end(function(err, res) {
-						$ = cheerio.load(res.text);
-						animeImage = $("div.crop.portrait img").attr("data-src");
-						console.log(animeImage);
-						imagesHtml += "<img src='http://www.anime-planet.com" + animeImage + "'><br>";
-					});
-				//Return the anime title string
-				return $(this).text();
-			}).get().join(", ");
-		});
+	var user_agent = {"User-Agent": "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14"};
+	var request = HTTP.call("GET", url, user_agent);
+	$ = cheerio.load(request.content);
+	var animeCards = $('div.anime-card').find("h3").map(function(i, el) {
+		var animeName = $(this).text();
+		var airTime = $(this).siblings().find("div.poster-wrap div.episode-countdown").text();
+		var animeEntry = new Anime(animeName, airTime);
+		animeArray.push(animeEntry);
+	});
 });
 
 Meteor.methods({
 	getChartData: function () {
-		for (var key in cardsObj) {
-			animeHtml += "<li>" + key + "<br>" + cardsObj[key] + "</li><hr>";
+		for (var i = 0; i < animeArray.length; i++) {
+			console.log("\nThe anime name is " + animeArray[i].animeName);
+			console.log("The airing time is " + animeArray[i].airTime);
 		}
-		return animeHtml + imagesHtml;
+		return animeHtml;
 	}
 });
