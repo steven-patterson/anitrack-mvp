ba10f30343453c592c72a04554aedb9d17980bc7
Steven Patterson <spatterson.dev@gmail.com>
2015-08-09 17:45:35 -0400

    Added comments

--- server/modules/chartModules.js
+++ server/modules/chartModules.js
@@ -13,26 +13,33 @@ var formatHtml = function (animeName, airTime) {
 }
 
 Meteor.startup(function() {
+	//Send request for data from anime schedule source
 	var url = "https://www.livechart.me/schedule/tv";
 	var user_agent = {"User-Agent": "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14"};
 	var request = HTTP.call("GET", url, user_agent);
-
+	//Load received data
 	$ = cheerio.load(request.content);
+	//Traverse and scrape the name and airing time
 	var animeCards = $('div.anime-card').find("h3").map(function(i, el) {
 		var animeName = $(this).text();
 		var airTime = $(this).siblings().find("div.poster-wrap div.episode-countdown").text();
+		//Store in an object and push to array
 		var animeEntry = new Anime(animeName, airTime);
 		animeArray.push(animeEntry);
 	});
+
 });
 
 Meteor.methods({
 	getChartData: function () {
+		//Loop through all anime entries
 		for (var i = 0; i < animeArray.length; i++) {
 			var aTitle = animeArray[i].animeName;
 			var aTime = animeArray[i].airTime;
+			//Format entry as html and add to string
 			animeHtml += formatHtml(aTitle, aTime);
 		}
+		//Return full anime list html
 		return animeHtml;
 	}
 });
