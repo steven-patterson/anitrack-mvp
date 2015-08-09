d10ca21c281f10203dde547cc462804428659640
Steven Patterson <spatterson.dev@gmail.com>
2015-08-09 17:42:47 -0400

    Created html formatting of titles

--- server/modules/chartModules.js
+++ server/modules/chartModules.js
@@ -8,10 +8,15 @@ function Anime (animeName, airTime) {
 	this.airTime = airTime;
 }
 
+var formatHtml = function (animeName, airTime) {
+	return "<li>" + animeName + "<br />" + airTime + "</li><hr />";
+}
+
 Meteor.startup(function() {
 	var url = "https://www.livechart.me/schedule/tv";
 	var user_agent = {"User-Agent": "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14"};
 	var request = HTTP.call("GET", url, user_agent);
+
 	$ = cheerio.load(request.content);
 	var animeCards = $('div.anime-card').find("h3").map(function(i, el) {
 		var animeName = $(this).text();
@@ -24,8 +29,9 @@ Meteor.startup(function() {
 Meteor.methods({
 	getChartData: function () {
 		for (var i = 0; i < animeArray.length; i++) {
-			console.log("\nThe anime name is " + animeArray[i].animeName);
-			console.log("The airing time is " + animeArray[i].airTime);
+			var aTitle = animeArray[i].animeName;
+			var aTime = animeArray[i].airTime;
+			animeHtml += formatHtml(aTitle, aTime);
 		}
 		return animeHtml;
 	}
