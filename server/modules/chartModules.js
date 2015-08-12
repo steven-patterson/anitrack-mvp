6dcbca52bae0e5a756d199e5b49aa9579b0552ba
Steven Patterson <spatterson.dev@gmail.com>
2015-08-12 02:52:31 -0400

    Added more restrictions on image fetch

--- server/modules/chartModules.js
+++ server/modules/chartModules.js
@@ -19,13 +19,38 @@ var imageFetch = function(animeName) {
 	}, headers: {"User-Agent": "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14"}});
 	$ = cheerio.load(request.content);
 	var url = $("#canvas").find("a").attr("href");
-	if (url !== undefined && url.indexOf("myanimelist" !== -1)) {
-		var animePicture = $("#canvas").find("img").attr("src");
-		return animePicture;
-	} else {
-		var animePicture = null;
+	var imageUrl = $(".item a").attr("href");
+	var imageString = String(imageUrl);
+	var placeHolder = "http://placehold.it/225x321";
+	try {
+		var imageStatus = HTTP.call("GET", imageUrl);
+		if (imageStatus.statusCode === 200) {
+			if (imageUrl !== undefined) {
+				if (imageString.indexOf("cdn.myanimelist.net/images/anime") !== -1) {
+					var animePicture = imageString;
+					return animePicture;
+				} else {
+					var animePicture = placeHolder;
+					return animePicture;
+				}
+			} else {
+				var animePicture = placeHolder;
+				return animePicture;
+			}
+		}
+	} catch(error) {
+		console.log(error);
+		var animePicture = placeHolder;
 		return animePicture;
 	}
+
+	// if (url !== undefined && url.indexOf("myanimelist" !== -1)) {
+	// 	var animePicture = $("#canvas").find("img").attr("src");
+	// 	return animePicture;
+	// } else {
+	// 	var animePicture = null;
+	// 	return animePicture;
+	// }
 }
 
 var formatHtml = function (animeName, airTime, animePicture) {
