5f38191768d433a14ebc38787058b21a587b7f3b
Steven Patterson <spatterson.dev@gmail.com>
2015-08-12 23:27:40 -0400

    Better image fetching function

--- server/modules/chartModules.js
+++ server/modules/chartModules.js
@@ -9,39 +9,22 @@ function Anime (animeName, airTime) {
 
 var imageFetch = function(animeName) {
 	var request = HTTP.call("GET", "http://www.bing.com/images/search", {params: {
-		"q": animeName + " myanimelist",
+		"q": String(animeName) + " anime",
 		"qs": "n",
 		"form": "QBIR",
-		"pq": animeName + " myanimelist",
+		"pq": String(animeName) + " anime",
 		"sc": "0-19",
 		"sp": "-1",
 		"sk": ""
 	}, headers: {"User-Agent": "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14"}});
 	$ = cheerio.load(request.content);
-	var url = $("#canvas").find("a").attr("href");
-	var imageUrl = $(".item a").attr("href");
-	var imageString = String(imageUrl);
-	var placeHolder = "http://placehold.it/225x321";
-	try {
-		var imageStatus = HTTP.call("GET", imageUrl);
-		if (imageStatus.statusCode === 200) {
-			if (imageUrl !== undefined) {
-				if (imageString.indexOf("cdn.myanimelist.net/images/anime") !== -1) {
-					var animePicture = imageString;
-					return animePicture;
-				} else {
-					var animePicture = placeHolder;
-					return animePicture;
-				}
-			} else {
-				var animePicture = placeHolder;
-				return animePicture;
-			}
-		}
-	} catch(error) {
-		console.log(error);
-		var animePicture = placeHolder;
+	var placeHolder = "http://placehold.it/230x170";
+	var animePicture = $("#canvas").find("img").attr("src");
+
+	if (animePicture !== undefined) {
 		return animePicture;
+	} else {
+		return placeHolder;
 	}
 }
 
